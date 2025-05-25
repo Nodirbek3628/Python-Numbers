8dee1f0d7765023b3dd5249d2ca24f8f975305d9
Nodirbek3628 <abloqulovnodirbek4@gmail.com>
2025-05-26 00:22:00 +0500

    6-vazifa bajarilidi

--- electricity_bill_calculator.py
+++ electricity_bill_calculator.py
@@ -1,1 +1,10 @@
 
+
+oy_boshi=float(input("Oy boshidagi istimolni kiriting:(kwt)"))
+oy_oxiri=float(input("Oy oxiridgi istimolni kiriting:(kwt)"))
+kwt=0.45 #1kwt narxi
+istimol=oy_oxiri-oy_boshi
+umumiy_tulov=kwt*istimol
+
+
+print(f'umumiy tulov:${umumiy_tulov:.2f}',)
