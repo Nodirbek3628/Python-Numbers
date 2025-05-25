c30a4c7a8e0113c10f46686bf6333a891484b84b
Nodirbek3628 <abloqulovnodirbek4@gmail.com>
2025-05-25 23:25:57 +0500

    4-vazifa bajarildi

--- taxi_fare_calculator.py
+++ taxi_fare_calculator.py
@@ -1,1 +1,11 @@
+from num2words import num2words
+
+boshlangich_tulov=float (input("boshlangich tulovni kiriting: ($)"))
+km_uchun=float(input("Har bir km uchun tulovni kiriting:($)"))
+masofani_km=float(input("Yurgan masofani kiriting:(km)"))
+
+umumiy_tulov=boshlangich_tulov+km_uchun*masofani_km
+
+print(umumiy_tulov,num2words(umumiy_tulov,lang="en"),",",num2words(umumiy_tulov,lang="ru"))
+
 
