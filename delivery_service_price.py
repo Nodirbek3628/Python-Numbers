227e328b4d081dedda8f0bbccd2d9ebcfd14eab6
Nodirbek3628 <abloqulovnodirbek4@gmail.com>
2025-05-26 00:03:17 +0500

    5-vazifa bajarildi

--- delivery_service_price.py
+++ delivery_service_price.py
@@ -1,1 +1,6 @@
-
+from num2words import num2words
+a=float(input("boshlangish tulovni kiriting:($)"))
+b=float(input("har bir (km) uchun tulovni kiriting:($)"))
+c=float(input("yurgan masofani kiriting:(km) "))
+yetkazib_berish=a+b*c
+print(f"umumiy tulov:${yetkazib_berish:.2f}",num2words(yetkazib_berish,lang="en",to="currency"),num2words(yetkazib_berish,lang="ru",to="currency"))
