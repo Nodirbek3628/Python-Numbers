c87e86c438ede630e7c75b6a97a76d16748b9403
Nodirbek3628 <abloqulovnodirbek4@gmail.com>
2025-05-25 22:14:46 +0500

    2-vazifa bajarildi

--- fast_food_price_rounder.py
+++ fast_food_price_rounder.py
@@ -1,1 +1,12 @@
+from num2words import num2words
 
+a=float(input("birinchi maxsulot narxini  kiriting>>($)"))
+b=float(input("ikkinch maxsulot narxini  kiriting>>($)"))
+c=float(input("uchunchi maxsulot narxini  kiriting>>($)"))
+
+umumiy_narxi=(a+b+c)
+a=f"${umumiy_narxi:.2f}"
+round=( umumiy_narxi,0.1)
+
+
+print(umumiy_narxi,(num2words(umumiy_narxi,lang="en",to="currency"),num2words(umumiy_narxi,lang="ru",to="currency")))
