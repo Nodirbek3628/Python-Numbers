e36891c58f01afdfa402f7bb32e4792f083721de
Nodirbek3628 <abloqulovnodirbek4@gmail.com>
2025-05-25 22:52:32 +0500

    3-vazifa bajarildi

--- average_spending_tracker.py
+++ average_spending_tracker.py
@@ -1,1 +1,13 @@
+from num2words import num2words
 
+
+a=float(input("birinchi qiymatni kiriting>>($),"))
+b=float(input("ikkinchi qiymatni kiriting>>($),"))
+c=float(input("uchunchi qiymatni kiriting>>($),"))
+d=float(input("turtinchi  qiymatni kiriting>>($),"))
+e=float(input("beshinchi qiymatni kiriting>>($),"))
+j=float(input("oltinchi qiymatni kiriting>>($),"))
+k=float(input("yettinchi qiymatni kiriting>>($),"))
+ortachasi=(a+b+c+d+e+j+k)/7
+
+print(ortachasi,num2words(ortachasi,lang="en",to="currency"),num2words(ortachasi,lang="ru",to="currency"))
